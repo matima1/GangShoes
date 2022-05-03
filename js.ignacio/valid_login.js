1cb4b5d995bf0c8cd7036b744c0de3381acf1780
Nach0Du0c <101341469+Nach0Du0c@users.noreply.github.com>
2022-05-03 00:52:57 -0400

    a

--- js.ignacio/valid_login.js
+++ js.ignacio/valid_login.js
@@ -1,106 +1,63 @@
-const formulario = document.getElementById('formulario__register');
-const inputs = document.querySelectorAll('#formulario__register input');
-
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-}
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+  password: /^.{4,12}$/, // 4 a 12 digitos.
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+};
 
-const campos = {
-	usuario: false,
-	nombre: false,
-	password: false,
-	correo: false,
-	telefono: false
-}
+//REGISTRO VALIDACIONES
+const form = document.getElementById("formulario__register");
+var nom = document.getElementById("nombre");
+var apel = document.getElementById("apellido");
+var cRegistro = document.getElementById("correoregistro");
+var tel = document.getElementById("telefono");
+var pRegistro = document.getElementById("passwordregistro");
+var msj = document.getElementById("mensaje");
 
-const validarFormulario = (e) => {
-	switch (e.target.name) {
-		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
-		break;
-		case "nombre":
-			validarCampo(expresiones.nombre, e.target, 'nombre');
-		break;
-		case "password":
-			validarCampo(expresiones.password, e.target, 'password');
-			validarPassword2();
-		break;
-		case "password2":
-			validarPassword2();
-		break;
-		case "correo":
-			validarCampo(expresiones.correo, e.target, 'correo');
-		break;
-		case "telefono":
-			validarCampo(expresiones.telefono, e.target, 'telefono');
-		break;
-	}
-}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos[campo] = true;
-	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos[campo] = false;
-	}
-}
+  let mensajesMostrar = "";
+  let entrarRegistro = false;
 
-const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
+  if (
+    nom.value.length < 3 ||
+    (nom.value.length > 10) & (apel.value.length < 3) ||
+    apel.value.length > 10 & (cRegistro.value.length < 0)
+  ) {
+    mensajesMostrar += "Longitud invalida, intente nuevamente <br>";
+    entrarRegistro = true;
+  }
+  // Con esto validamos que la letra sea mayuscula del nombre y el apellido
 
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
-	} else {
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
-	}
-}
+  // NOMBRE
+  var letraInicialNombre = nom.value.charAt(0);
+  if (!esMayuscula(letraInicialNombre)) {
+    mensajesMostrar += "La primera letra es minúscula del Nombre <br>";
+    entrarRegistro = true;
+  }
+  // APELLIDO
+  var letraInicialApellido = apel.value.charAt(0);
+  if (!esMayuscula(letraInicialApellido)) {
+    mensajesMostrar += "La primera letra es minúscula del Apellido <br>";
+    entrarRegistro = true;
+  }
+  // CORREO
 
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
+  var letraCorreoRegistro = cRegistro.value.charAt(0);
+  if (!esMayuscula(letraCorreoRegistro)) {
+    mensajesMostrar += "Registro no ingresado";
+	entrarRegistro = true;
+  }
+
+  // ENVIAR FORMULARIO REGISTRO
+  if (entrarRegistro) {
+    msj.innerHTML = mensajesMostrar;
+  } else {
+    msj.innerHTML = "Formulario Enviado";
+  }
 });
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
-		formulario.reset();
-
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 5000);
-
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
-	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-	}
-});
+// Con esto validamos que sea una mayuscula la letra
+function esMayuscula(letra) {
+  return letra == letra.toUpperCase();
+}
