24a66ac80c064d310b3f487ccf70d677646a48a1
Nach0Du0c <101341469+Nach0Du0c@users.noreply.github.com>
2022-05-03 14:45:12 -0400

    cometario

---
+++ js.ignacio/valid_comentario.js
@@ -0,0 +1,23 @@
+//              COMENTARIO VALIDACIONES
+const formComentario = document.getElementById("form-comentario");
+var com = document.getElementById("com1");
+var msjCom = document.getElementById("msjComentario");
+
+
+formComentario.addEventListener("submit", (a) => {
+    a.preventDefault();
+  let mensaje = "";
+  let enviadoComentario = false;
+  if (com.value.length < 20 || com.value.length > 2000) {
+    a.preventDefault();
+    mensaje +=
+      "Longitud del Comentario no valido, intente nuevamente <br>";
+    enviadoComentario = true;
+  }
+  //              ENVIAR COMENTARIO
+  if (enviadoComentario) {
+    msjCom.innerHTML = mensaje;
+  } else {
+    msjCom.innerHTML = "Enviado";
+  }
+});
--- js.ignacio/valid_login.js
+++ js.ignacio/valid_login.js
@@ -1,44 +1,53 @@
+
 //REGISTRO VALIDACIONES
 const form = document.getElementById("formulario__register");
+
 var nom = document.getElementById("nombre");
 var apel = document.getElementById("apellido");
-
-var expReg = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-var cRegistro = expReg.test("correoregistro");
 var correoRegistro = document.getElementById("correoregistro");
 
 var tel = document.getElementById("telefono");
-var pRegistro = document.getElementById("passwordregistro");
-var correo = document.getElementById("mail").value; 
+var passR = document.getElementById("passwordregistro");
+var passR2 = document.getElementById("passwordregistro2");
+
+var msj = document.getElementById("mensaje");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let mensajeNombreApellido = "";
-
+  let msjMostrar = "";
+  let expReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
   let entrarRegistro = false;
 
   //         NOMBRE
-  if (nom.value.length < 3 || nom.value.length > 10 & apel.value.length < 3 || apel.value.length > 10) {
-    mensajeNombreApellido += "Longitud invalida del Nombre o Apellido, intente nuevamente <br>";
+  if (nom.value.length < 2 || nom.value.length > 10) {
+    msjMostrar += "Longitud invalida del Nombre, intente nuevamente <br>";
     entrarRegistro = true;
   }
+  //         APELLIDO
+  if (apel.value.length < 2 || apel.value.length > 10) {
+    msjMostrar += "Longitud invalida del Apellido, intente nuevamente <br>";
+    entrarRegistro = true;
+  }
+  //          CORREO
+  if (!expReg.test(correoRegistro.value)) {
+    msjMostrar += "El email no valido <br>";
+    entrarRegistro = true;
+  }
+
   // Con esto validamos que la letra sea mayuscula del nombre y el apellido
   var letraInicialNombre = nom.value.charAt(0);
   var letraInicialApellido = apel.value.charAt(0);
   if (!esMayuscula(letraInicialNombre) || !esMayuscula(letraInicialApellido)) {
-    mensajeNombreApellido += "La primera letra es minúscula del Nombre o Apellido <br>";
+    msjMostrar += "La primera letra es minúscula del Nombre o Apellido <br>";
     entrarRegistro = true;
   }
 
-
-
-
   // ENVIAR FORMULARIO REGISTRO
   if (entrarRegistro) {
-    msj.innerHTML = mensajeNombreApellido;
+    msj.innerHTML = msjMostrar;
   } else {
-    msj.innerHTML = "Formulario Enviado";
+    msj.innerHTML = "Enviado";
   }
 });
 // Con esto validamos que sea una mayuscula la letra
