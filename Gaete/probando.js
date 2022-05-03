0d0bde9f31c85fdc6175f97647cbeff172fa18f7
unknown <i.perez@duocuc.cl>
2022-05-03 11:12:49 -0400

    asask

--- Gaete/probando.js
+++ Gaete/probando.js
@@ -1,18 +1,37 @@
-$(document).ready(function(){
-    $("formulariomio").submit(function(e){
-        e.preventDefault();
-        let mensajesMostrar="";
-        let entrar=false;
+//CONTACTO VALIDACIONES
+const form = document.getElementById("formulariomio");
+var nom = document.getElementById("nombre");
 
-        var nombre=$("#nombre").val();
-        if(nombre.trim().length<4 || nombre.trim().lenght>7){
-            mensajesMostrar+="La longitud no es correcta";
-            entrar=true;
-        }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    });
+  let mensajeNombreApellido = "";
 
+  let contacto = false;
+
+  //         NOMBRE
+  if (nom.value.length < 3 || nom.value.length > 10 & apel.value.length < 3 || apel.value.length > 10) {
+    mensajeNombreApellido += "Longitud invalida del Nombre o Apellido, intente nuevamente <br>";
+    contacto = true;
+  }
+  // Con esto validamos que la letra sea mayuscula del nombre y el apellido
+  var letraInicialNombre = nom.value.charAt(0);
+  if (!esMayuscula(letraInicialNombre) || !esMayuscula(letraInicialApellido)) {
+    mensajeNombreApellido += "La primera letra es minúscula del Nombre o Apellido <br>";
+    contacto = true;
+  }
+
+
+
+
+  // ENVIAR FORMULARIO REGISTRO
+  if (contacto) {
+    msj.innerHTML = mensajeNombreApellido;
+  } else {
+    msj.innerHTML = "Formulario Enviado";
+  }
 });
-
-
-
+// Con esto validamos que sea una mayuscula la letra
+function esMayuscula(letra) {
+  return letra == letra.toUpperCase();
+}
